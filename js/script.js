1edc32855008a8a62d3b64e4713c51fb703a5df6
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-01-30 22:32:53 +0200

    Gallery generation script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-//----------------------- MODAL WINDOW SHOW/HIDE ---------------//
 
+import imagesObjArray from "./imagesArray.js"
+//----------------------- MODAL WINDOW SHOW/HIDE ---------------//
+{
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -14,9 +16,9 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-
+}
 //------------------------------ MOBILE MENU SHOW/HIDE ------------------//
-
+{
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
@@ -28,15 +30,13 @@
     mobileMenuRef.classList.toggle("is-open");
   })
 })();
-
+}
 //---------------------------- GALLERY MENU HANDLER  ------------------//
 
-const menuWrapRef = document.querySelectorAll('.menu-item-wrap');
+{const menuWrapRef = document.querySelectorAll('.menu-item-wrap');
 const menuListItem = Array.from(document.querySelectorAll('.menu-cat-button'));
-
 function menuItemOpen(event) {
   const targetItem = event.target.nextElementSibling.classList;
-  console.log(event.target.parentNode);
   if (!targetItem.contains('isOpen')) {
     menuWrapRef.forEach(item => item.classList.remove('isOpen'));
     targetItem.add('isOpen');
@@ -44,13 +44,37 @@ function menuItemOpen(event) {
 
 };
 menuListItem.forEach(item => item.addEventListener('click', menuItemOpen));
-
+}
 //------------------------------- DAY/NIGHT STYLE SWITCH --------------------//
-
+{
 const nightModeSwitch = document.querySelector('.nightMode-checkbox');
-
 function nightModeToggle() {
   document.querySelector('body').classList.toggle('light-theme');
 }
 nightModeSwitch.addEventListener('change', nightModeToggle);
+}
+//-------------------------------  GALLERY PAGE IMAGES GENERATION  -------------------//
 
+const galleryPageRef = document.querySelector('.gallery-page');
+function galleryImgRender(imagesObjArray) {
+  const imgRenderArray = imagesObjArray.map(el => {
+    return `<div class="gallery-page-wrap">
+          <div class="gallery-page-img-wrap">
+              <img
+                class="gallery-page-img"
+                src="https://picsum.photos/id/${el.id}/250"
+                srcset="https://picsum.photos/id/${el.id}/250 1x, https://picsum.photos/id/${el.id}/500 2x"
+                alt="image"
+              />
+          </div>
+          <div class="gallery-page-text">
+            <h2 class="gallery-page-header">${el.author}</h2>
+            <p class="gallery-page-paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            </p>
+          </div>
+        </div>`;
+  });
+  galleryPageRef.insertAdjacentHTML('afterbegin', imgRenderArray.join(''));
+  }
+galleryImgRender(imagesObjArray);
