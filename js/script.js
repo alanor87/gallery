20985cf3785a4846b25942365a922b11b69f5908
alanor87 <70328441+alanor87@users.noreply.github.com>
2021-02-06 12:28:01 +0200

    Gallery modal navigation
    Plus responsive design of this navigation. Only clickable arrows for now.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const modalImgWindowRef = document.querySelector('[data-img-modal]');
 const modalUploadRef = document.querySelector('[data-upload-modal]');
 const modalNavRef = document.querySelector('[data-nav-modal]');
 const modalImgRef = document.querySelector('.modal-image');
+const modalImgNav = document.querySelectorAll('.image-nav-arrow')
 const galleryPageRef = document.querySelector('.gallery-page');
 const galleryMenu = document.querySelector('.gallery-menu');
 const backdropRef = Array.from(document.querySelectorAll('.backdrop'));
@@ -16,8 +17,13 @@ const menuListItem = document.querySelector('.gallery-menu');
 const nightModeSwitch = document.querySelector('.nightMode-checkbox');
 const sideMenuSwitch = document.querySelector('.sideMenu-checkbox');
 
+
+/* ----------------------------*/
+/*  GALLERY RENDERING FUNCTION */
+/* ----------------------------*/
+
 function galleryImgRender(imagesObjArray) {
-  const imgRenderArray = imagesObjArray.map(el => {
+  const imgRenderArray = imagesObjArray.map((el, index) => {
     return `<div class="gallery-page-wrap">
           <div class="gallery-page-img-wrap">
               <img
@@ -26,18 +32,23 @@ function galleryImgRender(imagesObjArray) {
                 srcset="https://picsum.photos/id/${el.id}/250 1x, https://picsum.photos/id/${el.id}/500 2x"
                 alt="image"
                 data-src="https://picsum.photos/id/${el.id}/1000"
+                data-index="${index}"
               />
           </div>
           <div class="gallery-page-text">
             <h2 class="gallery-page-header">${el.author}</h2>
             <p class="gallery-page-paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.</br> Index : ${index}. 
             </p>
           </div>
         </div>`;
   });
   galleryPageRef.insertAdjacentHTML('afterbegin', imgRenderArray.join(''));
 };
+
+/* ------------------------------------------*/
+/*  MODAL WINDOW OPEN/CLOSE TRIGGER HANDLERS */
+/* ------------------------------------------*/
 
 function openModal(event) {
   const role = event.currentTarget.dataset.modOpenTrigger;
@@ -88,6 +99,10 @@ function closeModal(event) {
   }
 };
 
+/* ----------------------------------------------------*/
+/*  SPECIFIC MODAL WINDOWS OPEN/CLOSE TOGGLE FUNCTIONS */
+/* ----------------------------------------------------*/
+
 function navToggleHandler() {
   modalNavRef.classList.toggle("modal-hidden");
 };
@@ -106,9 +121,14 @@ function imgToggleHandler(event) {
   };
   if (target.classList.contains('gallery-page-img')) {
     modalImgRef.src = target.dataset.src;
+    modalImgRef.dataset.index = target.dataset.index;
     modalImgWindowRef.classList.toggle("modal-hidden");
   };
 };
+
+/* -------------------------------------------*/
+/*  SIDE MENU ITEM OPEN/CLOSE TOGGLE FUNCTION */
+/* -------------------------------------------*/
 
 function sideMenuItemOpen(event) {
   if (!event.target.classList.contains('menu-cat-button')) return;
@@ -117,11 +137,36 @@ function sideMenuItemOpen(event) {
     menuWrapRef.forEach(item => item.classList.remove('isOpen'));
     targetItem.add('isOpen');
   } else targetItem.remove('isOpen');
-
 };
+
+/* ----------------------------*/
+/*  NIGHT MODE TOGGLE FUNCTION */
+/* ----------------------------*/
+
 function nightModeToggle() {
   document.querySelector('body').classList.toggle('light-theme');
 };
+
+function modalImageNav(indexShift) {
+  const currentIndex = Number(modalImgRef.dataset.index);
+  const nextIndex = currentIndex + indexShift;
+  if (nextIndex < 0 || nextIndex >= imagesObjArray.length) return;
+  modalImgRef.src = `https://picsum.photos/id/${imagesObjArray[nextIndex].id}/1000`;
+  modalImgRef.dataset.index = nextIndex;
+}
+
+function modalImgTriggerHandler(event) {
+  const direction = event.target.dataset.modalImgNav;
+  switch (direction) {
+    case 'prev':
+      modalImageNav(-1);
+      break;
+    case 'next':
+       modalImageNav(1);
+      break;
+  }
+}
+
 
 galleryImgRender(imagesObjArray);
 nightModeSwitch.addEventListener('change', nightModeToggle);
@@ -129,3 +174,4 @@ sideMenuSwitch.addEventListener('change', () => { galleryMenu.classList.toggle('
 menuListItem.addEventListener('click', sideMenuItemOpen);
 modalOpenTriggersRef.forEach(item => item.addEventListener("click", openModal));
 modalCloseTriggersRef.forEach(item => item.addEventListener("click", closeModal));
+modalImgNav.forEach(item => item.addEventListener("click", modalImgTriggerHandler));
